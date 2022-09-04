79a3fe1f3a5aebb4dd845024bc586bedd9a3af38
Fahmi Erza <112784307+fahmierza@users.noreply.github.com>
2022-09-04 10:02:07 +0700

    Create souce code.py

---
+++ souce code.py
@@ -0,0 +1,175 @@
+
+from djitellopy import Tello
+import cv2
+import numpy as np
+
+width = 640 
+height = 480  
+deadZone =10
+roll, pitch, updown, yaw = 0,0,0,0
+
+startCounter = 0
+
+me = Tello()
+me.connect()
+me.for_back_velocity = 0
+me.left_right_velocity = 0
+me.up_down_velocity = 0
+me.yaw_velocity = 0
+me.speed = 25
+me.set_speed(25)
+me.send_control_command("speed {}".format(25))
+
+print(me.get_battery())
+
+me.streamoff()
+me.streamon()
+
+frameWidth = width
+frameHeight = height
+
+global imgContour
+def empty(a):
+    pass
+
+cv2.namedWindow("HSV")
+cv2.resizeWindow("HSV",320,240)
+cv2.createTrackbar("HUE Min","HSV",0,179,empty)
+cv2.createTrackbar("HUE Max","HSV",10,179,empty)
+cv2.createTrackbar("SAT Min","HSV",150,255,empty)
+cv2.createTrackbar("SAT Max","HSV",255,255,empty)
+cv2.createTrackbar("VALUE Min","HSV",50,255,empty)
+cv2.createTrackbar("VALUE Max","HSV",255,255,empty)
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters",320,240)
+cv2.createTrackbar("Threshold1","Parameters",80,255,empty)
+cv2.createTrackbar("Threshold2","Parameters",81,255,empty)
+cv2.createTrackbar("Area","Parameters",4000,30000,empty)
+
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img,imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        areaMin = cv2.getTrackbarPos("Area", "Parameters")
+        if area > areaMin:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x , y , w, h = cv2.boundingRect(approx)
+            cx = int(x + (w / 2))  
+            cy = int(y + (h / 2))  
+            dir0 = ((cx*(0.105263))+(0-31.78947))*2.12
+            dir0 = int(dir0)
+            dir1 = (cy*(0-0.12961))+(28.437394)
+            dir1 = int(dir1)
+            dir2 = (h*(0-2.07805))+(181.7338)
+            dir2 = int(dir2)
+            print("center X = ", cx, " | center Y = ", cy, " | height = ", h, " | pitch = ", dir2, " | updown = ", dir1, " | yaw = ", dir0)
+
+
+            cv2.line(imgContour, (int(frameWidth/2),int(frameHeight/2)), (cx,cy),(0, 0, 255), 3)
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,(0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,(0, 255, 0), 2)
+            cv2.putText(imgContour, " " + str(int(x)) + " " + str(int(y)), (x - 20, y - 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0, 255, 0), 2)
+            
+            if dir2 > -20:
+                pitch = 20
+            elif dir2 < -70:
+                pitch = -20
+            else:
+                pitch = 0
+            
+            yaw = dir0
+            updown = dir1 - 2
+            roll = 0
+            
+            me.send_rc_control(roll, pitch, updown, yaw)
+            print(pitch, updown, yaw)
+            
+
+def display(img):
+    cv2.line(img,(int(frameWidth/2)-deadZone,0),(int(frameWidth/2)-deadZone,frameHeight),(255,255,0),3)
+    cv2.line(img,(int(frameWidth/2)+deadZone,0),(int(frameWidth/2)+deadZone,frameHeight),(255,255,0),3)
+    cv2.circle(img,(int(frameWidth/2),int(frameHeight/2)),5,(0,0,255),5)
+    cv2.line(img, (0,int(frameHeight / 2) - deadZone), (frameWidth,int(frameHeight / 2) - deadZone), (255, 255, 0), 3)
+    cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone), (255, 255, 0), 3)
+
+while True:
+
+    frame_read = me.get_frame_read()
+    myFrame = frame_read.frame
+    img = cv2.resize(myFrame, (width, height))
+    imgContour = img.copy()
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("HUE Min","HSV")
+    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHsv,lower,upper)
+    result = cv2.bitwise_and(img,img, mask = mask)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    getContours(imgDil, imgContour)
+    display(imgContour)
+
+    if startCounter == 0:
+       me.takeoff()
+       me.send_rc_control(0,0,30,0)
+       startCounter = 1
+
+    stack = stackImages(0.6, ([img, result], [imgDil, imgContour]))
+    cv2.imshow('Horizontal Stacking', stack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        me.land()
+        break
+
+cv2.destroyAllWindows()
